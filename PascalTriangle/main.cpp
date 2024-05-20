62df4573c63f353aab89fa2154c86d0e60c27679
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-05-20 15:10:12 +0300

    1

--- PascalTriangle/main.cpp
+++ PascalTriangle/main.cpp
@@ -5,10 +5,13 @@ using std::cout;
 using std::endl;
 
 #define WIDTH   8
+#define PASCAL_IS_TRIANGLE
+//#define PASCAL_IS_TRIANGLE_1
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef PASCAL_IS_TRIANGLE
 	int h;
 	cout << "Введите высоту треугольника: "; cin >> h;
 	int nf = 1;
@@ -24,7 +27,7 @@ void main()
 		nf *= n;
 		for (int i = 0; i < h - n; i++)
 		{
-			cout.width(WIDTH/2);
+			cout.width(WIDTH / 2);
 			cout << "";
 		}
 		int mf = 1;
@@ -38,11 +41,15 @@ void main()
 			{
 				mnf *= mn;
 			}
-			cout.width(WIDTH); // задаём ширину вывода, а именно, сколько знакопозиций будет занимать выводимое значение.
+			cout.width(WIDTH); //задаем ширину вывода, а именно, сколько знакопозиций будет занимать выводимое значение.
+			//cout.width() воздействует только на одно выводимое значение, для следующего выводимого значения, опять нужно задавать ширину.
 			cout << nf / mf / mnf;
 		}
 		cout << endl;
-#ifdef PASCAL_IS_TRIANGLE
+	}
+#endif // PASCAL_IS_TRIANGLE
+
+#ifdef PASCAL_IS_TRIANGLE_1
 		int i, j, k;
 		int numbers, lines;
 		cout << "Введите сколько будет рядов: ";
@@ -62,5 +69,5 @@ void main()
 			cout << endl << endl;
 		}
 #endif 
-	}
 }
+
